9572c9856364028ae6414a972550c512faf3477c
jianke <1340289@qq.com>
2019-12-10 11:18:12 +0800

    一个小测测试

---
+++ employee3/tes01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
+
+#include<stdio.h>
+
+int main(){
+
+printf("ceshi \n");
+
+return 0;
+
+}
